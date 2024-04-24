a72f16c75dd39e005daa34a698c2fed99e79d50a
Vikas Rana <159464524+vikas0128@users.noreply.github.com>
2024-04-24 12:41:25 +0530

    Update ga.java

--- ga.java
+++ ga.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-class GuessTheNumber_vikas{
+class Task1{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
